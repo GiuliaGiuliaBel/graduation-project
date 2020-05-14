64c091549aea7872089917291d6a13f596ee53f0
Giulia Giulia <GiuliaGiuliaBel@gmail.com>
2020-05-15 01:05:01 +0300

    add popupMenu

--- src/modules/showBurgerMenu.js
+++ src/modules/showBurgerMenu.js
@@ -1,15 +1,37 @@
 const showBurgerMenu = () => {
 
    const hiddenLarge = document.querySelector('.hidden-large'),
-      hiddenSmall = document.querySelector('.hidden-small');
+      hiddenSmall = document.querySelector('.hidden-small'),
+      popupMenu = document.querySelector('.popup-menu'),
+      closeMenuBtn = document.querySelector('.close-menu-btn');
+   
+      if(document.documentElement.clientWidth < 768){     
+         hiddenSmall.style.display = "none";
+         hiddenLarge.style.display = "block";
+      }
    
    window.addEventListener('resize', () => {
     if (document.documentElement.clientWidth < 768) {
-         hiddenSmall.style.display = 'none';
-         hiddenLarge.style.display = 'block';
+         hiddenSmall.style.display = "";
+         hiddenLarge.style.display = "block";
     } else {
-         hiddenLarge.style.display = '';   
-         hiddenSmall.style.display = 'flex';
+         hiddenLarge.style.display = "";   
+         hiddenSmall.style.display = "flex";
+      }
+   });
+
+   hiddenLarge.addEventListener('click', (event) => {
+      popupMenu.style.display = "flex";
+      console.log(event.target);
+   });
+
+   closeMenuBtn.addEventListener('click', () => {
+      popupMenu.style.display = "none";    
+   });
+
+   popupMenu.addEventListener('click', event => {    
+      if(event.target.tagName === "A"){
+         popupMenu.style.display = "none"; 
       }
    });
 };
--- src/modules/showModal.js
+++ src/modules/showModal.js
@@ -4,13 +4,13 @@ const showModal = () => {
         closeForm = document.querySelector('.close-form'),
         freeVisitForm = document.getElementById('free_visit_form');
 
-         freeVisit.addEventListener('click', () => {
-         freeVisitForm.style.display = "block";
+        freeVisit.addEventListener('click', () => {
+        freeVisitForm.style.display = "block";
      });
      
      closeForm.addEventListener('click', () => {
       console.log(2);  
-     })
+     });
 };
 
 export default showModal;
