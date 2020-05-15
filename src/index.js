47e6d38a881fe8b538ec6e9be41ba1a5f683ae4a
Giulia Giulia <GiuliaGiuliaBel@gmail.com>
2020-05-15 17:03:09 +0300

    update

--- src/index.js
+++ src/index.js
@@ -6,21 +6,14 @@ import showModal from './modules/showModal';
 import scrollToTop from './modules/scrollToTop';
 import sliderGallery from './modules/sliderGallery';
 import sendForm from './modules/sendForm';
-<<<<<<< HEAD
 import mainSlider from './modules/mainSlider';
 import popupClose from './modules/popupClose';
-=======
->>>>>>> 502cbde65f8cc0f84dc37aacabd0e164a800a5c3
 
 operateBurgerMenu();
 showDropDownChooseClub();
 showModal();
 scrollToTop();
 sliderGallery();
-<<<<<<< HEAD
 mainSlider();
 sendForm('banner-form');
 popupClose();
-=======
-sendForm('banner-form');
->>>>>>> 502cbde65f8cc0f84dc37aacabd0e164a800a5c3
--- src/modules/popupClose.js
+++ src/modules/popupClose.js
@@ -1,5 +1,4 @@
 const popupClose = () => {
-<<<<<<< HEAD
     const popupCloseButtons = document.querySelectorAll('.close_icon');
 
     popupCloseButtons.forEach(item => item.addEventListener('click', event => { 
@@ -7,12 +6,6 @@ const popupClose = () => {
        if(event.target.closest('.popup')) {
             event.target.closest('.popup').style.display = "none";
        };        
-=======
-    const popupCloseButtons = document.querySelectorAll('.close-icon');
-
-    popupCloseButtons.forEach(item => item.addEventListener('click', event => {
-        event.currentTarget.parentNode.style.visibility = 'hidden';
->>>>>>> 502cbde65f8cc0f84dc37aacabd0e164a800a5c3
     }));
 };
 
